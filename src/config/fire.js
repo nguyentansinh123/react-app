5ef49429fefcda95820e1c6de2737affb01eb178
nguyentansinh123 <66555356+nguyentansinh123@users.noreply.github.com>
2021-08-30 00:57:49 +0700

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAZPMGpQkKd6n1MB1GA68jkiQTsC0x_fyk",
-    authDomain: "react-app-c4dff.firebaseapp.com",
-    projectId: "react-app-c4dff",
-    storageBucket: "react-app-c4dff.appspot.com",
-    messagingSenderId: "59583292825",
-    appId: "1:59583292825:web:00795aad60b076924e5f08"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   const fire = firebase.initializeApp(firebaseConfig)
 
